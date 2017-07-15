b77382ea5d19568984a2329953fad7dc34126e3c
akshayshahane <akshay.shahane13@gmail.com>
2017-07-15 23:00:46 +0530

    feat : add leak canary
    A memory leak detection library for Android and Java

---
+++ app/src/main/java/com/example/android/bakingapp/application/BakingApplication.java
@@ -0,0 +1,23 @@
+package com.example.android.bakingapp.application;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * Created by akshayshahane on 15/07/17.
+ */
+
+public class BakingApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+        // Normal app init code...
+    }
+}
